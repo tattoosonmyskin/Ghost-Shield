5b3ddbbc3863ea33071d5cacd168ee247c71aab7
alphareasoning <alphareasoning@AlphaOS.lan>
2026-05-15 10:24:34 -0500

    patch: total canvas/audio muxing to break static hash 4aea2810 [0x00000000bd6a2950]

--- extension/inject.js
+++ extension/inject.js
@@ -1,33 +1,48 @@
-const generateHex = () => '0x' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
-
-Object.defineProperty(navigator, 'hardwareConcurrency', { get: () => Math.floor(Math.random() * 8) + 2 });
-Object.defineProperty(navigator, 'deviceMemory', { get: () => 16 });
-
-const originalGetParameter = WebGLRenderingContext.prototype.getParameter;
-WebGLRenderingContext.prototype.getParameter = function(p) {
-    if (p === 0x9245) return "Ghost Engine Virtualized Interface";
-    if (p === 0x9246) return `Sovereign_GPU_MUX [${generateHex()}]`;
-    return originalGetParameter.apply(this, arguments);
-};
-
-// Ghost-Shield: High-Entropy Vector Inversion [0x00000000bd6a2500]
 (function() {
-    const originalGetImageData = CanvasRenderingContext2D.prototype.getImageData;
-    CanvasRenderingContext2D.prototype.getImageData = function(x, y, w, h) {
-        const imageData = originalGetImageData.apply(this, arguments);
-        // Mutate 1-bit of stochastic noise in the first pixel buffer
-        // This alters the CRC32 Hash without inducing visible UI disfluency
-        imageData.data[0] = imageData.data[0] ^ (Math.random() > 0.5 ? 1 : 0);
-        return imageData;
+    const poison = () => (Math.random() > 0.5 ? 1 : -1);
+    
+    // Total Canvas Overwrite: getImageData, toDataURL, and toBlob
+    const wrap = (obj, prop, wrapper) => {
+        const original = obj[prop];
+        obj[prop] = function() {
+            return wrapper.apply(this, [original, arguments]);
+        };
     };
 
-    // Obfuscate AudioBuffer Channel Data (The 'Voice' vector)
-    const originalGetChannelData = AudioBuffer.prototype.getChannelData;
-    AudioBuffer.prototype.getChannelData = function() {
-        const data = originalGetChannelData.apply(this, arguments);
+    // Poison 2D Context readback
+    wrap(CanvasRenderingContext2D.prototype, 'getImageData', function(orig, args) {
+        const image = orig.apply(this, args);
+        image.data[0] = image.data[0] ^ poison(); // 1-bit stochastic jitter
+        return image;
+    });
+
+    // Poison string-based readback (toDataURL/toBlob)
+    // We subtly draw a nearly-invisible pixel before every export
+    const distort = (canvas) => {
+        const ctx = canvas.getContext('2d');
+        if (ctx) {
+            const val = ctx.getImageData(0, 0, 1, 1);
+            val.data[0] = val.data[0] ^ poison();
+            ctx.putImageData(val, 0, 0);
+        }
+    };
+
+    wrap(HTMLCanvasElement.prototype, 'toDataURL', function(orig, args) {
+        distort(this);
+        return orig.apply(this, args);
+    });
+
+    wrap(HTMLCanvasElement.prototype, 'toBlob', function(orig, args) {
+        distort(this);
+        return orig.apply(this, args);
+    });
+
+    // Poison AudioContext Frequency response
+    wrap(AudioBuffer.prototype, 'getChannelData', function(orig, args) {
+        const data = orig.apply(this, args);
         for (let i = 0; i < data.length; i += 100) {
-            data[i] += (Math.random() - 0.5) * 1e-7; // Basal entropy injection
+            data[i] += (Math.random() - 0.5) * 1e-7;
         }
         return data;
-    };
+    });
 })();
