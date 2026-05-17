91b2a2de2acf1a8a874827aaf8a22184b07675e7
alphareasoning <alphareasoning@AlphaOS.lan>
2026-05-16 19:14:08 -0500

    patch: masking raw specs to target intel profile standard [0x00000000bd6a3300]

--- extension/inject.js
+++ extension/inject.js
@@ -1,31 +1,19 @@
 (function() {
     'use strict';
 
-    // Randomized Hardware Concurrency Spoof
-    Object.defineProperty(navigator, 'hardwareConcurrency', {
-        get: () => 8
-    });
+    // Mask CPU core count to a generic baseline
+    Object.defineProperty(navigator, 'hardwareConcurrency', { get: () => 4 });
+    Object.defineProperty(navigator, 'deviceMemory', { get: () => 8 });
 
-    // Randomized Device Memory Spoof
-    Object.defineProperty(navigator, 'deviceMemory', {
-        get: () => 16
-    });
-
-    // WebGL Vendor and Renderer Randomization Loop
+    // Mask WebGL Vendor & Renderer to a common standard profile (Intel/NVIDIA)
     const originalGetParameter = WebGLRenderingContext.prototype.getParameter;
     WebGLRenderingContext.prototype.getParameter = function(parameter) {
-        // UNMASKED_VENDOR_WEBGL
-        if (parameter === 37445) {
-            return "Google Inc. (AMD)";
-        }
-        // UNMASKED_RENDERER_WEBGL
-        if (parameter === 37446) {
-            return "ANGLE (AMD, AMD Radeon 610M (radeonsi raphael_mendocino LLVM 19.1.7), OpenGL 4.6)";
-        }
+        if (parameter === 37445) return "Intel Open Source Technology Center";
+        if (parameter === 37446) return "Mesa DRI Intel(R) UHD Graphics (ADL GT2)";
         return originalGetParameter.apply(this, arguments);
     };
 
-    // AudioContext Spoofing Sequence (Successfully verified in Run 3)
+    // Keep the working AudioContext noise filter from Run 4
     const originalGetChannelData = AudioBuffer.prototype.getChannelData;
     AudioBuffer.prototype.getChannelData = function() {
         const data = originalGetChannelData.apply(this, arguments);
